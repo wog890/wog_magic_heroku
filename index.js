5eb89af790ce95471388fb303ed1710dab7b547e
wog890 <wogcarver@gmail.com>
2016-11-16 17:47:29 -0500

    Created code to start websocket

--- index.js
+++ index.js
@@ -1,20 +1,3 @@
-var express = require('express');
-var app = express();
+var websocket = require('./src/wog/websocket');
 
-app.set('port', (process.env.PORT || 5000));
-
-app.use(express.static(__dirname + '/public'));
-
-// views is directory for all template files
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
-
-app.get('/', function(request, response) {
-  response.render('pages/index');
-});
-
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
-});
-
-
+websocket.init();
